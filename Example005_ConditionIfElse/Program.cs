269ae48e0ddc095e8fd07f651f1a9226e507ac0a
KarpovaOV <olikarpovabusuu@yandex.ru>
2023-08-11 17:52:17 +0400

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿string username = Console.ReadLine();
+
+if(username == "Оля")
+{
+    Console.WriteLine("Ура, это же Оля");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
