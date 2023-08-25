ad866ac79c69dbfb28797d0361de656d4714f18d
KarpovaOV <olikarpovabusuu@yandex.ru>
2023-08-25 22:36:01 +0400

    lt4#2

---
+++ lt4#2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+int[] MyArray(int[] array)
+{
+    // int[] array = new int[8];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0,2);
+        
+        System.Console.Write(array[i] + " | ");
+
+    }
+    return array;
+}
+
+int[] array = new int[8];
+
+MyArray(array);
+
