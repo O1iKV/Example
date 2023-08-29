a4f322bfb9592a618aae6f76fb78150eaa8edc8e
KarpovaOV <olikarpovabusuu@yandex.ru>
2023-08-29 21:25:39 +0400

    DZ4_2

---
+++ DZ4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//адача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Sum(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        int last_digit = number % 10;
+        sum = sum + last_digit;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int Parse(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int a = Parse("Введите число");
+
+int result = Sum(a);
+
+Console.WriteLine(result);
