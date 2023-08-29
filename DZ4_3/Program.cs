ecbbf89d8bef21beb007b928e4f112fa457d8233
KarpovaOV <olikarpovabusuu@yandex.ru>
2023-08-29 23:23:15 +0400

    DZ4_3

---
+++ DZ4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+  int count = array.Length;
+  int index = 0;
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(10);
+}
+  Console.Write("[");
+  while(index < count){
+    Console.Write(array[index]);
+    index++;
+    if (index < count){
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
